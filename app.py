329036e702d95bc70ff414aee213ef3283bba193
Felix Michaux <f.michaux@rcvs.org.uk>
2017-02-02 12:04:01 +0000

    Added DB

--- app.py
+++ app.py
@@ -4,9 +4,36 @@ from flask import Flask, render_template, redirect, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, required, Regexp
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+base_path = os.getcwd()
+db_path = os.path.join(base_path, 'TechChallenge.db')
 
 app = Flask(__name__)
 app.secret_key = 'skdjgsgdf8duh80bt8e8hg09resj9e'
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///'+str(db_path)
+
+db = SQLAlchemy(app)
+
+class Submissions(db.Model):
+    ID = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    FIRST_NAME = db.Column(db.Text, nullable=False)
+    LAST_NAME = db.Column(db.Text, nullable=False)
+    POSTCODE = db.Column(db.String(10), nullable=False)
+    AMOUNT = db.Column(db.Float, nullable=False)
+    REASON = db.Column(db.Text, nullable=False)
+
+    def __init__(self, ID, FIRST_NAME, LAST_NAME, POSTCODE, AMOUNT, REASON):
+        self.ID = ID
+        self.FIRST_NAME = FIRST_NAME
+        self.LAST_NAME = LAST_NAME
+        self.POSTCODE = POSTCODE
+        self.AMOUNT = AMOUNT
+        self.REASON = REASON
+
+    def __repr__(self):
+        return '<Submission %r>' % self.ID
 
 class grantApplication(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired()])
@@ -19,6 +46,9 @@ class grantApplication(FlaskForm):
 def index():
     form = grantApplication()
     if form.validate_on_submit():
+        submission = Submissions(None, str(form.first_name.data), str(form.last_name.data), str(form.postcode.data), float(form.money_amount.data), str(form.deliciousness.data))
+        db.session.add(submission)
+        db.session.commit()
         session['name'] = form.first_name.data
         return redirect('confirmation')
     else:
